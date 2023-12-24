3867c5f29c1f450ae479c632148a0816dc6baae1
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-24 20:59:34 +0530

    html done

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const expenseAmount = document.getElementById('expenseAmount').ariaValueMax;
+    const expenseName = document.getElementById('expenseName');
+    const expenseCategory = document.getElementById('expenseCategory');
+
+    
+});
